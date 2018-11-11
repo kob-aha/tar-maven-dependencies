b77b3b7447b958a0b48a53ae0a5d5b862dbb5c35
Koby Aharon <kobyahron@gmail.com>
2018-11-11 14:50:04 +0200

    Update tar-dependencies.sh to use and bundle a local maven repository

--- tar-dependencies.sh
+++ tar-dependencies.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
-DEPENDENCIES_OUTPUT_FILE=dependencies-output.txt
+set -o pipefail
 
-echo -n "Output dependencies list to file ... "
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LOCAL_REPO_DIR="${DIR}/maven-repo"
 
-output=$( ./mvnw -Doutput.file.name=${DEPENDENCIES_OUTPUT_FILE} dependency:go-offline 2>&1 ) 
+rm -rf ${LOCAL_REPO_DIR}
+
+echo -n "Download dependencies to ${LOCAL_REPO_DIR} ... "
+
+output=$( ./mvnw -Dmaven.repo.local=${LOCAL_REPO_DIR} dependency:go-offline 2>&1 | tee mvn_process.log ) 
 
 if [ $? -ne 0 ]; then
   echo "[Failed]"
   echo -e "Command output:\n ${output}"
+  exit 1
 else
   echo "[Done]"
 fi
 
 echo -n "Create dependencies tar file ... "
-output=$( awk -F':' '/\.jar/ { print $NF  }' ${DEPENDENCIES_OUTPUT_FILE} | tar -cvf dependencies.tar -T - 2>&1  )
+output=$( tar -cvf dependencies.tar ${LOCAL_REPO_DIR} 2>&1  )
 
 if [ $? -ne 0 ]; then
   echo "[Failed]"
   echo -e "Command output:\n ${output}"
+  exit 1
 else
   echo "[Done]"
 fi
